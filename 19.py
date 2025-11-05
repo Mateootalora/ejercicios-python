011e5fbce15b7d97e75bd1597b49573f6f4ddda4
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:33:41 -0500

    Potencias de un Número

---
+++ 19.py
@@ -0,0 +1,9 @@
+valor_base = float(input("Ingrese su numero: "))
+
+potencia_dos = valor_base ** 2
+potencia_tres = valor_base ** 3
+
+print("El cuadrado es:", potencia_dos)
+print("El cubo es:", potencia_tres)
+
+
