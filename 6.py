ba8866e3003ee023215f1e79c845d21709740822
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:25:33 -0500

    Media de Tres Números

---
+++ 6.py
@@ -0,0 +1,8 @@
+valor1 = float(input("Ingrese su primer numero: "))
+valor2 = float(input("Ingrese su segundo numero: "))
+valor3 = float(input("Ingrese su tercer numero: "))
+
+suma_total = valor1 + valor2 + valor3
+media = suma_total / 3
+
+print("El promedio de los tres numeros es:", media)
