e16bf14a26e323436047ce59d1f607b111e94fef
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:26:15 -0500

    Cálculo Salario Bruto

---
+++ 7.py
@@ -0,0 +1,6 @@
+horas_trabajadas = float(input("Ingrese el numero de horas: "))
+costo_por_hora = float(input("Ingrese el valor de cada hora: "))
+
+salario_total = horas_trabajadas * costo_por_hora
+
+print("El salario es:", salario_total)
