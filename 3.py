e538df588ec64234c70181314d1381909c9d3caf
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:23:50 -0500

    Cálculo de Módulo

---
+++ 3.py
@@ -0,0 +1,6 @@
+dividendo = float(input("Ingrese su primer numero (dividendo): "))
+divisor = float(input("Ingrese su segundo numero (divisor): "))
+
+modulo_resultado = dividendo % divisor
+
+print("El modulo de", dividendo, "y", divisor, "es:", modulo_resultado)
