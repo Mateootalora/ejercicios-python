9e9b368244cdb3462c1202503abf3b22dd8162cb
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:29:08 -0500

    Doble y Triple de un Número

---
+++ 12.py
@@ -0,0 +1,7 @@
+entrada = float(input("Ingrese su numero: "))
+
+calculo_doble = entrada * 2
+calculo_triple = entrada * 3
+
+print("El doble de su numero es:", calculo_doble)
+print("El triple de su numero es:", calculo_triple)
