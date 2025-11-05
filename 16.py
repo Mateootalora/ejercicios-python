f2f281dd2059fb57079dc90d41f58fd426165209
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:31:29 -0500

    Promedio de Cuatro Notas

---
+++ 16.py
@@ -0,0 +1,11 @@
+evaluacion1 = float(input("Dame tu nota 1: "))
+evaluacion2 = float(input("Dame tu nota 2: "))
+evaluacion3 = float(input("Dame tu nota 3: "))
+evaluacion4 = float(input("Dame tu nota 4: "))
+
+suma_notas = evaluacion1 + evaluacion2 + evaluacion3 + evaluacion4
+media_final = suma_notas / 4
+
+print("Tu promedio es:", media_final)
+
+
