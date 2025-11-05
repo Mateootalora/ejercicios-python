e3a9a86010d522f37a2969766bdcdb5904111ac7
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:30:55 -0500

    Geometría del Círculo

---
+++ 15.py
@@ -0,0 +1,11 @@
+import math
+
+r = float(input("Ingrese el radio del circulo: "))
+
+area_calculada = math.pi * math.pow(r, 2)
+circunferencia = 2 * math.pi * r
+
+print("El area del circulo es:", area_calculada)
+print("El perimetro (circunferencia) del circulo es:", circunferencia)
+
+
