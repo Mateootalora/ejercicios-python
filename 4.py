4ded19f2e8a2ab913ac41558648d56f9196ee7cd
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:24:32 -0500

    Aumento del 30%

---
+++ 4.py
@@ -0,0 +1,6 @@
+numero_original = float(input("Ingrese un numero del 0 al 200: "))
+
+FACTOR_AUMENTO = 1.30
+numero_aumentado = numero_original * FACTOR_AUMENTO
+
+print("Su numero aumentado un 30% es:", numero_aumentado)
