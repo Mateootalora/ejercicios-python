71c245c1ba230e6be67f3fe3202d1ed3b77d7d2d
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:27:17 -0500

    Operación Suma

---
+++ 9.py
@@ -0,0 +1,6 @@
+sumando1 = float(input("Ingrese su primer numero: "))
+sumando2 = float(input("Ingrese su segundo numero: "))
+
+resultado_suma = sumando1 + sumando2
+
+print("La suma de", sumando1, "y", sumando2, "es:", resultado_suma)
