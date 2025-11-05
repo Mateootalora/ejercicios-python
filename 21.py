c3e7da02fef97c351a161a1f98d299cacda64d90
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:34:54 -0500

    Descuento del 15%

---
+++ 21.py
@@ -0,0 +1,8 @@
+FACTOR_DESCUENTO = 0.85
+
+valor_inicial = float(input("Ingrese un numero del 0 al 50: "))
+valor_final = valor_inicial * FACTOR_DESCUENTO
+
+print("Su numero disminuido un 15% es:", valor_final)
+
+
