e5f15464f784b848390220f163f9f820d9d6ad5b
Mateootalora <monolocox84@gmail.com>
2025-11-05 07:34:12 -0500

    Área y Perímetro de Rectángulo

---
+++ 20.py
@@ -0,0 +1,12 @@
+ancho = float(input("Ingrese su base: "))
+alto = float(input("Ingrese su altura: "))
+
+area_calc = ancho * alto
+
+suma_lados = ancho + alto
+perim_calc = 2 * suma_lados
+
+print("El area del rectangulo es:", area_calc)
+print("El perimetro del rectangulo es:", perim_calc)
+
+
